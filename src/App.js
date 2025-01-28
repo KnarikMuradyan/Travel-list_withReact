98ea0998172c912e711a8fdffa3c8d8066dc5d8a
Knarik Muradyan <muradyan.knarik96@gmail.com>
2025-01-28 21:40:54 +0400

    Added hamdlerToggleItem  function for packed items

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ export default function App() {
 
   function handlerDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
+
   }
+
+  function handlerToggleItem(id){
+    setItems((items) =>items.map((item)=>item.id ===id?{...item,packed: !item.packed} :item))
+    
+  }
+
 
   return <div className="app">
     <Logo />
     <Form onAddItems={handleAddItems} />
-    <PackingList items={items} onDeleteItem={handlerDeleteItem} />
+    <PackingList items={items} onDeleteItem={handlerDeleteItem} onToggleItem={handlerToggleItem} />
     <Stats />
 
   </div>
@@ -67,14 +74,14 @@ function Form({ onAddItems }) {
   )
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem,onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
           <Item key={item.id}
             item={item}
-            onDeleteItem={onDeleteItem} />
+            onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
         ))}
       </ul>
     </div>
@@ -82,9 +89,10 @@ function PackingList({ items, onDeleteItem }) {
   )
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem ,onToggleItem}) {
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={()=>{onToggleItem(item.id)}}/>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description} </span>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
