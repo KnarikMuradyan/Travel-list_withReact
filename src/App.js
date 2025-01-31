d7a162401152b1a8d5d73e26ee0a524d5dbd9e99
Knarik Muradyan <muradyan.knarik96@gmail.com>
2025-01-31 22:12:20 +0400

    Added new XML and made it functional

--- src/App.js
+++ src/App.js
@@ -41,9 +41,6 @@ function Form({ onAddItems }) {
   const [quantity, setQuantity] = useState(1);
   const [description, setDescription] = useState("");
 
-
-
-
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -56,7 +53,6 @@ function Form({ onAddItems }) {
 
     setQuantity(1);
     setDescription("");
-
 
   }
 
@@ -75,15 +71,37 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem,onToggleItem }) {
+
+  const[sortBy,setSortBy] =useState("input");
+
+  let sortedItems;
+
+  if(sortBy==="input") sortedItems=items;
+
+  if(sortBy==="description") sortedItems=items
+  .slice()
+  .sort((a,b)=>a.description.localeCompare(b.description))
+
+if (sortBy==="packed") sortedItems=items.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item key={item.id}
             item={item}
             onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
         ))}
       </ul>
+
+      <div className="actions" >
+        <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed ststus</option>
+        </select>
+
+      </div>
     </div>
 
   )
@@ -106,7 +124,7 @@ function Stats({items}) {
   if(!items.length) return (<p className="stats">
     <em>Start adding some items to your packing list 🚀</em>
   </p>);
-  
+
   const numItems =items.length;
   const numPacked =items.filter((item)=>item.packed).length;
   const percentage = numItems > 0 ? Math.round((numPacked / numItems) * 100) : 0;
